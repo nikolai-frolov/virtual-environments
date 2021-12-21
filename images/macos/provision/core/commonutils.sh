50ad8b7f5f2ee917ba7f181f8049a4390dda2a29
nikolai-frolov <nikolay.frolov@akvelon.com>
2021-12-21 14:58:52 +0300

    Attempt to install and uninstall macfuse

--- images/macos/provision/core/commonutils.sh
+++ images/macos/provision/core/commonutils.sh
@@ -16,6 +16,18 @@ for package in $cask_packages; do
     brew install --cask $package
 done
 
+# Install packages which should be removed after security preferences update 
+brew install macfuse
+
+# Execute AppleScript to change security preferences
+# System Preferences -> Security & Privacy -> General -> Unlock -> Allow -> Not now
+if is_BigSur; then
+    osascript $HOME/utils/confirm-identified-developers.scpt $USER_PASSWORD
+fi
+
+# Remove temporary packages
+brew uninstall macfuse
+
 # Specify Bazel version 3.7.1 to install due to the bug with 4.0.0: https://github.com/bazelbuild/bazel/pull/12882
 if is_Less_Catalina; then
     export USE_BAZEL_VERSION="3.7.1"
